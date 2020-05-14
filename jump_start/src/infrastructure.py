69ab0263c9e3d178ba8cc8c79ff611042ebfbda5
Philip Bove <phil@bove.online>
2020-05-14 19:53:21 -0400

    working on dnsmasq all in one container and HTTP serving container

--- jump_start/src/infrastructure.py
+++ jump_start/src/infrastructure.py
@@ -1,14 +1,7 @@
 from jump_start.src.exceptions import ConfigException
 import jump_start.src.data as data
 from jinja2 import Environment, BaseLoader
-import dns.zone
-import dns.exception
-import dns.name
-import dns.rdatatype
-import dns.rdataclass
-import dns.zone
 import docker
-import dns
 import os
 import yaml
 import json
@@ -32,9 +25,11 @@ class InfraCont():
             os.mkdir(self.mnt_vol)
         self.volumes = {self.mnt_vol: {'bind': list(self.config['ContainerConfig']['Volumes'].keys())[0], 'mode':'Z'}}
 
-    def generate_config(self): pass
+    def generate_config(self):
+        pass
 
-    def check_config(self): pass
+    def check_config(self):
+        pass
 
     def run(self):
         self.pull()
@@ -63,53 +58,49 @@ class InfraCont():
             self.container.remove(force=True)
 
 
-class DHCPContainer(InfraCont):
+class DNSMasq(InfraCont):
 
-    def __init__(self, output, cont_name, client, dhcp_config):
+    cont_name = 'quay.io/bandit145/dnsmasq'
+
+    def __init__(self, output, client, dhcp_config, dns_config, pxe_config):
         self.dhcp_config = dhcp_config
+        self.dns_config = dns_config
+        self.pxe_config = pxe_config
         super().__init__(output, cont_name, client)
 
     def generate_config(self):
-        with open(self.mnt_vol + '/kea-dhcp4.conf', 'w') as cont_file:
-            json.dump(self.dhcp_config, cont_file)
+        config_file = ''
+        #global options
+        config_file += 'domain={0}\n'.format(dns_config['domain'])
+        config_file += 'no-hosts\n'
+        config_file += 'enable-tftp\n'
+        config_file += 'tftp-root=/etc/dnsmasq/tftp/\n'
+        #dhcp settings
+        for key, value in dhcp_config:
+            config_file += '{0}={1}\n'.format(key, value)
+        #dns settings
+        for key, vlaue in dns_config:
+            config_file += '{0}={1}\n'.format(key, value)
+        #pxe settings
+        config_file += 'dhcp-boot={0}'.format(pxe_config['boot-file'])
+        if not os.path.exits(self.mnt_vol + '/tftp/'):
+            os.symlink(pxe_config['tftp_dir'], self.mnt_vol + '/tftp/', target_is_directory=True)
+        with open(self.mnt_vol + 'dnsmasq.conf') as file:
+            file.write(config_file)
 
     def check_config(self):
         self.output.debug('checking dhcp confg')
-        self.output.debug('DHCP volumes ' + str(self.volumes))
-        output = self.client.containers.run(self.cont_name, '/usr/sbin/kea-dhcp4 -t ' + self.volumes[self.mnt_vol]['bind'] + '/kea-dhcp4.conf', volumes=self.volumes, stderr=True, remove=True, detach=True)
-        self.output.debug(output)
+        self.output.debug('DHCP volumes {0}'.format(str(self.volumes)))
+        output = self.client.containers.run(self.cont_name, '/usr/sbin/dnsmasq -d /etc/dnsmasq/dnsmasq.conf --test')
+        self.output.debug('DHCP config check output:\n{0}'.format(output))
 
 
+class Http(InfraCont):
+    cont_name = 'quay.io/bandit145/apache'
 
-class DNSContainer(InfraCont):
-
-    def __init__(self, output, cont_name, client, dns_zone, dns_config=None):
-        self.dns_zone = dns_zone
-        self.dns_config = dns_config
-        
-        if dns_config:
-            self.dns_config = dns_config
-        super().__init__(output, cont_name, client)
-
+    def __init__(self, output, http_config):
+        self.http_config = http_config
+        super().__init__(output, Http.cont_name, client)
+    # TODO: add util configuration classes/functions to get certain OS/s, self configure edition
     def generate_config(self):
-        template = Environment(loader=BaseLoader).from_string(data.named_config)
-        with open(self.mnt_vol + '/named.conf', 'w') as named_conf:
-            named_conf.write(template.render(isc_config=self.dns_config))
-        os.chmod(self.mnt_vol + '/named.conf', 0o777)
-
-        zone = dns.zone.Zone(dns.name.from_text(self.dns_zone['name']))
-        # create soa
-        soa_dataset = zone.find_rdataset(self.dns_zone['name'], dns.rdatatype.SOA, create=True)
-        soa_rdata = dns.rdata.from_text(dns.rdataclass.IN, dns.rdatatype.SOA, self.dns_zone['master_name'] + ' ' + self.dns_zone['responsible_name'] + ' 0' + ' ' + str(self.dns_zone['refresh']) + ' ' + str(self.dns_zone['retry']) + ' ' + str(self.dns_zone['expire']) + ' 0')
-        soa_dataset.add(soa_rdata, self.dns_zone['ttl'])
-        for record in self.dns_zone['records']:
-            rdtype = dns.rdatatype.from_text(record['type'].upper())
-            record_data = dns.rdata.from_text(dns.rdataclass.IN, rdtype, ' '.join(record['data']))
-            new_rec = zone.get_rdataset(record['name'], rdtype, create=True)
-            if 'ttl' in record.keys():
-                new_rec.add(record_data, record['ttl'])
-            else:
-                new_rec.add(record_data, self.dns_zone['ttl'])
-        with open(self.mnt_vol + '/' + self.dns_zone['name'] + '.db', 'w') as zone_conf:
-            zone.to_file(zone_conf)
-
+        pass
